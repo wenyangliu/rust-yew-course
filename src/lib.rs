f39cddf515190c8f1083aabdb1e1db7cdc8b18e1
liuxiang <1753439422@qq.com>
2022-04-27 15:31:33 +0800

    feat: HTML Loops

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,9 @@ pub fn app() -> Html {
     let class = "my_title";
     // let message = Some("I am a message");
     let message: Option<&str> = None;
+
+    let tasks = vec!["record video", "grocery shopping", "pet Dog"];
+
     html! {
         <>
           <h1 class={class}>{"Hello World!!!"}</h1>
@@ -31,6 +34,14 @@ pub fn app() -> Html {
           } else {
             <p>{"no message to see today"}</p>
           }
+
+          <ul>
+            {list_to_html(tasks)}
+          </ul>
         </>
     }
+}
+
+fn list_to_html(list: Vec<&str>) -> Vec<Html> {
+  list.iter().map(|item| html!{<li>{item}</li>}).collect()
 }
