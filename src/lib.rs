2f457afd55f3afe30ed1b5b88dcae38b57a0cb90
liuxiang <1753439422@qq.com>
2022-04-27 15:19:02 +0800

    feat: HTML Conditionals

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,22 @@ pub fn app() -> Html {
     log!("my name is", name);
     log!(serde_json::to_string_pretty(&my_object).unwrap());
     let class = "my_title";
+    // let message = Some("I am a message");
+    let message: Option<&str> = None;
     html! {
         <>
           <h1 class={class}>{"Hello World!!!"}</h1>
-          <p>{"Hi there!"}</p>
+          if class == "my_titles" {
+            <p>{"Hi there!"}</p>
+          } else {
+            <p>{"I'm not a titles!"}</p>
+          }
+
+          if let Some(message) = message {
+            <p>{message}</p>
+          } else {
+            <p>{"no message to see today"}</p>
+          }
         </>
     }
 }
