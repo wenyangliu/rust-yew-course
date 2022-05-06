8a73cc61968e79d48027d48feb09a2c527b6e77a
liuxiang <1753439422@qq.com>
2022-05-06 15:53:46 +0800

    feat: Programatic Linking to Pages

--- src/components/pages/hello.rs
+++ src/components/pages/hello.rs
@@ -1,8 +1,17 @@
 use yew::prelude::*;
+use yew_router::{history, prelude::*};
+use crate::router::Route;
 
 #[function_component(Hello)]
 pub fn hello() -> Html {
+  let history = use_history().unwrap();
+  let onclick = Callback::from(move |_| {
+    history.push(Route::Home);
+  });
   html! {
-    <h1>{"Hello"}</h1>
+    <div>
+      <h1>{"Hello"}</h1>
+      <button {onclick}>{"Go Home"}</button>
+    </div>
   }
 }
